356102f53013919a4f06897dcad2b50795ef720b
Akash <akashmane12598@gmail.com>
2020-10-24 01:32:22 +0530

    [Akaah] Add. Master Code

---
+++ BSTDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BSTDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to BST code!");
+        }
+    }
+}
