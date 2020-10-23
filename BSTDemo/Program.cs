e0f43a89b17e78bc94617e6d7af00bafa589aabe
Akash <akashmane12598@gmail.com>
2020-10-24 01:45:05 +0530

    [Akaah] Add. Search Elements in BST UC3

--- BSTDemo/Program.cs
+++ BSTDemo/Program.cs
@@ -22,6 +22,9 @@ namespace BSTDemo
             binarySearch.Insert(67);
             binarySearch.Display();
             binarySearch.GetSize();
+
+            bool result = binarySearch.IfExists(63, binarySearch);
+            Console.WriteLine(result);
         }
     }
 }
