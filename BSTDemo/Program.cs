dbcd1f6275f6c75bcaaffa6e5e448ad19173d7ab
Akash <akashmane12598@gmail.com>
2020-10-24 01:37:09 +0530

    [Akaah] Add. Creating BST UC1

--- BSTDemo/Program.cs
+++ BSTDemo/Program.cs
@@ -7,6 +7,11 @@ namespace BSTDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BST code!");
+            Binary<int> binarySearch = new Binary<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            binarySearch.GetSize();
         }
     }
 }
